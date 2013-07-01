2fcb3cbb9bd6fd2e4859cb7dc9c314594f7a3ff6
Russen Guggemos <russen@promoboxx.com>
2013-07-01 10:52:15 -0400

    Init feature commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '62fd28a1b4146074418f9b0f4e180734697620697d51c1702f01460d0d9c2211c21fd656b995853fe953201da5a9ef74bbcbe0d916ee351694bf44866a3efd14'
